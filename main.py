0bd412f7745c58e094de35922e3b96e0e56d149f
Glitch2210 <MichaelSciortino89@gmail.com>
2025-06-26 22:07:31 -0400

    updated main.py added count_words_in_block function

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def get_book_text(file_path):
     #takes a file path and returns contents as string
     with open(file_path) as f:
         return f.read()
+    
+def count_words_in_block(book_block):
+    #takes a book_block string and counts the number of words by spliting it then ranging it
+    words = book_block.split()
+    word_count = len(words)
+    return f"{word_count} words found in the document"
 
 def main():
     #variable block
@@ -12,7 +18,7 @@ def main():
     book_block = get_book_text(book_file_path)
 
     #prints book_block
-    print(book_block)
+    print(count_words_in_block(book_block))
     return
 
 main()
