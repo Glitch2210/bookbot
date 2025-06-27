1b548ba7a7cc2409dad67b405077bb48abe3819a
Glitch2210 <MichaelSciortino89@gmail.com>
2025-06-27 07:49:06 -0400

    add file path input functionality

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
+import sys
 from stats import count_words_in_block 
 from stats import count_char_in_block
+from stats import char_count_sort
 
 def get_book_text(file_path):
     #takes a file path and returns contents as string
-    with open(file_path) as f:
-        return f.read()
+    with open(file_path) as x:
+        return x.read()
     
 def main():
     #variable block
     book_block = ""
-    book_file_path = "books/frankenstein.txt"
+    
+    if len(sys.argv) == 2:
+        book_file_path = sys.argv[1]
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        raise sys.exit(1)
+    
+        
 
     #populate book_block by book_file_path
     book_block = get_book_text(book_file_path)
+    print_report(book_file_path,book_block)
+    return
 
+def print_report(book_file_path,book_block):
     #prints book_block
+    sorted_chars = []
+    sorted_chars = char_count_sort(count_char_in_block(book_block))
+    
+    #prints report
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_file_path}")
+    print("----------- Word Count ----------")
     print(count_words_in_block(book_block))
-    print(count_char_in_block(book_block))
+    print("--------- Character Count -------")
+
+    #prints the sorted_char list and filters non isalpha
+    for i in sorted_chars:
+        
+        if i["char"].isalpha():
+            c = i["char"]
+            n = i["num"]
+            line = f"{c}: {n}"
+            print(line)
+    print("============= END ===============")
+
+
     return
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def count_words_in_block(book_block):
     #takes a book_block string and counts the number of words by spliting it then ranging it
     words = book_block.split()
     word_count = len(words)
-    return f"{word_count} words found in the document"
+    return f"Found {word_count} total words"
 
 def count_char_in_block(book_block):
     #takes a book_block string and counts the number of chars in it
@@ -16,3 +16,16 @@ def count_char_in_block(book_block):
             char_count_dictionary[c] = 1
 
     return char_count_dictionary
+
+def sort_on(items):
+    return items["num"]
+
+def char_count_sort(char_count_dictionary):
+    #sorts the char count dictionary into a sorted list of dictionaries
+    library_chars = []
+    
+    for char in char_count_dictionary:
+            char_dic = {"char":char,"num":char_count_dictionary[char]}
+            library_chars.append(char_dic)
+    library_chars.sort(reverse=True, key=sort_on)
+    return library_chars 
